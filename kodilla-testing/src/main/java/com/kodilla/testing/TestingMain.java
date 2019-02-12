0ed64c3e077a4c8ecb9448a3736c1d81d5c90eac
DeoreXT <askalski@1lobrodnica.pl>
2019-02-12 12:18:42 +0100

    First commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,34 +5,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator calculator = new Calculator();
-
-        int a = 5, b = 2;
-
-        if (calculator.add(a, b) == a + b) {
-            System.out.println("test OK");
-        }
-        else {
-            System.out.println("Error method add()!");
-        }
-
-        if (calculator.subtract(a, b) == a - b) {
-            System.out.println("Test OK");
-        }
-        else {
-            System.out.println("Error method subtract()gitt!");
-        }
     }
 }
