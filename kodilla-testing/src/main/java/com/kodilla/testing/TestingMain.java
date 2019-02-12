6a052d1398976bec151d00a6dd0f6392d397e2ca
DeoreXT <askalski@1lobrodnica.pl>
2019-02-12 11:44:14 +0100

    First commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int a = 5, b = 2;
+
+        if (calculator.add(a, b) == a + b) {
+            System.out.println("test OK");
+        }
+        else {
+            System.out.println("Error method add()!");
+        }
+
+        if (calculator.subtract(a, b) == a - b) {
+            System.out.println("Test OK");
+        }
+        else {
+            System.out.println("Error method subtract()gitt!");
+        }
     }
 }
