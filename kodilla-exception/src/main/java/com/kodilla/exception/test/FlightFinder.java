655ba39ea9c263145d3d675152ae72abb3f47e4d
DeoreXT <askalski@1lobrodnica.pl>
2019-02-28 00:43:25 +0100

    28.02

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -8,8 +8,8 @@ public class FlightFinder {
         Map<String, Boolean> airportAvailability = new HashMap<String, Boolean>();
         boolean arrivalAirport = false, departureAirport = false;
         for (Map.Entry<String, Boolean> aa : airportAvailability.entrySet()) {
-            if (flight.getArrivalAirport() == aa.getKey() && aa.getValue() == true) arrivalAirport = true;
-            if (flight.getDepartureAirport() == aa.getKey() && aa.getValue() == true) departureAirport = true;
+            if (flight.getArrivalAirport().equals(aa.getKey()) && aa.getValue() == true) arrivalAirport = true;
+            if (flight.getDepartureAirport().equals(aa.getKey()) && aa.getValue() == true) departureAirport = true;
         }
         if (!arrivalAirport || !departureAirport) throw new RouteNotFoundException();
         return flight;
